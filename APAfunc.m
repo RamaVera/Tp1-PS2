79d62808698a65206415def62ea7b60d6bfda990
Ramiro Vera <rverag@fi.uba.ar>
2019-10-01 01:25:41 -0300

    .Update point 4

--- APAfunc.m
+++ APAfunc.m
@@ -1,4 +1,4 @@
-function [e, y, w]=APAfunc(u, d, M, mu, O)
+function [e, y, w, w_n]=APAfunc(u, d, M, mu, O)
 %u input
 %d signal to estimate
 %M adaptative filter order
@@ -11,13 +11,14 @@ function [e, y, w]=APAfunc(u, d, M, mu, O)
     u=[zeros(M,1); u];
     y=zeros(N,1);
     e=zeros(N,1);
+    w_n=zeros(M, N);
     for k=1:N-(O-1)
         X=getApaMatrix(u,k,M,O);
         D=d((k-1)+O:-1:(k-1)+1);
         E=D-X*w;      
         Y=X*w;
+        w_n(:, k) = w;
         w=w+mu*X'*inv(epsilon*eye(O,O)+X*X')*E;
-        
         e(k)=mean(E);
         y(k)=mean(Y);
     end
--- ex4.m
+++ ex4.m
@@ -1,12 +1,13 @@
+
+item='2';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%    Ejercicio 4
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Rapidez de Convergencia
+if(strcmp(item,'1'))
 close all
 clear all
 clc
-
-%Ejercicio 4
-
-close all; 
-clear all; 
-
 mu = 0.1;
 M = 11;
 r = 5;
@@ -45,3 +46,49 @@ legend(legends)
 ylim([0 1]);
 xlim([0 1000]);
 print('JForAllFilters','-dpng')
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Rapidez ante cambios
+if(strcmp(item,'2'))
+
+close all; 
+clear all; 
+
+M = 11;
+mu = 0.0075;
+r = 5;
+N = 10000;
+L = 5;
+W1 = 2.9;
+W2 = 3.4;
+noise_variance = 0.001;
+num_experiments = 20;
+
+LMSBuff={'lms',[0.02,0,0]};
+NLMSBuff={'nlms',[1,0,0]};
+RLSBuff={'rls',[0,1,0]};
+APABuff={'apa',[1,0,10]};
+FilterBuffer={LMSBuff,NLMSBuff,RLSBuff,APABuff};
+
+for j=1 : length(FilterBuffer)
+    filterType=FilterBuffer{j}{1};
+    params=FilterBuffer{j}{2};
+    J_estimation = zeros(N, num_experiments);
+    for i=1 : num_experiments
+        [d1, u1] = build_signals(r, N/2, L, W1, noise_variance);
+        [d2, u2] = build_signals(r, N/2, L, W2, noise_variance);
+        sum(abs(u1).^2);
+        sum(abs(u2).^2);
+        u = [u1 ; u2];
+        d = [d1 ; d2];
+        [e, y, w_eq, w_n]= adapFilterfunc(filterType, u, d, M, params);
+        J_estimation(:, i) = abs(e).^2;
+    end
+    J_estimation2(:,j) =  mean(J_estimation, 2);
+end
+
+figure();
+hold on;
+plot((J_estimation2));
+end
